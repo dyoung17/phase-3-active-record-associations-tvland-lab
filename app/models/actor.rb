a08dae3dd5bca11214f4356d1b5225beede1833c
Daven Young <davenyoung@msn.com>
2022-05-24 15:22:36 -0600

    commit tree bcecbe6ff70738b2a0e07fad681dc1945f3b2231

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :networks
+    
+    def characters
+        self.characters
+    end
+    def shows
+        self.shows
+    end
+    def full_name
+        fullname=self.first_name + self.last_name
+    end
+    def list_roles
+        self.characters.map{|c| c.name}
+    end
 end
